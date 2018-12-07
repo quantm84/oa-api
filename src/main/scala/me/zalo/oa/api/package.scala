348511eeeb8299f9651fe974cee41f8e021a87d8
quantm84 <quantm84@gmail.com>
2018-12-07 11:48:45 +0700

    refactor

--- src/main/scala/me/zalo/oa/api/package.scala
+++ src/main/scala/me/zalo/oa/api/package.scala
@@ -7,11 +7,30 @@ import scalaj.http.Http
 
 package object api {
 
+  private val urlPrefix = "https://openapi.zalo.me/v2.0/oa"
   private val printer = Printer(preserveOrder = true, dropNullValues = true, indent = "")
 
+  /**
+   * Send a text message to follower.
+   * [[https://developers.zalo.me/docs/api/official-account-api/api/gui-tin-nhan-post-2343]]
+   *
+   * @param uid         user id for oa
+   * @param msg         message
+   * @param accessToken access token
+   * @return (response code, response body)
+   */
   def sendTextMessage(uid: String, msg: String, accessToken: String): (Int, String) =
     sendMessage(uid, Message(msg, None), accessToken)
 
+  /**
+   * Send a message to follower.
+   * [[https://developers.zalo.me/docs/api/official-account-api/api/gui-tin-nhan-post-2343]]
+   *
+   * @param uid         user id for oa
+   * @param msg         message
+   * @param accessToken access token
+   * @return
+   */
   def sendMessage(uid: String, msg: Message, accessToken: String): (Int, String) = {
     val data = Json.obj(
       "recipient" -> Json.obj(
@@ -19,7 +38,7 @@ package object api {
       ),
       "message" -> msg.asJson
     )
-    val response = Http(s"https://openapi.zalo.me/v2.0/oa/message?access_token=$accessToken")
+    val response = Http(s"$urlPrefix/message?access_token=$accessToken")
       .header("Content-Type", "application/json")
       .postData(printer.pretty(data))
       .asString
